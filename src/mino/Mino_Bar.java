d75700e6fb97f1a77204e41940a762905ca5233a
gabi-b612 <20km095@esisalama.org>
2023-12-26 05:38:24 +0200

    Correction de la position x,y

--- src/mino/Mino_Bar.java
+++ src/mino/Mino_Bar.java
@@ -50,11 +50,11 @@ public class Mino_Bar extends Mino{
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
 
-        tempB[1].x = b[0].x - Block.SIZE;
-        tempB[1].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
 
-        tempB[2].x = b[0].x + Block.SIZE;
-        tempB[2].y = b[0].y;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
 
         tempB[3].x = b[0].x;
         tempB[3].y = b[0].y + Block.SIZE * 2;
