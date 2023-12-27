376f998f6443d02dc6c61c8c96627f9c41a6b7c2
gabi-b612 <20km095@esisalama.org>
2023-12-27 04:07:16 +0200

    Refactor de update Check the movement lors de la collision Check the rotation lors de la collision

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -18,32 +18,35 @@ public class Mino {
 
 
     public void create(Color c) {
-        b[0] = new Block(c);
-        b[1] = new Block(c);
-        b[2] = new Block(c);
-        b[3] = new Block(c);
-        tempB[0] = new Block(c);
-        tempB[1] = new Block(c);
-        tempB[2] = new Block(c);
-        tempB[3] = new Block(c);
+        this.b[0] = new Block(c);
+        this.b[1] = new Block(c);
+        this.b[2] = new Block(c);
+        this.b[3] = new Block(c);
+        this.tempB[0] = new Block(c);
+        this.tempB[1] = new Block(c);
+        this.tempB[2] = new Block(c);
+        this.tempB[3] = new Block(c);
     }
 
     public void setXY(int x, int y) {};
 
     public void updateXY (int direction) {
-        this.direction = direction;
-        b[0].x = tempB[0].x;
-        b[0].y = tempB[0].y;
+        this.checkRotationCollision();
+        if (!leftCollision && !rightCollision  && !bottomCollision) {
+            this.direction = direction;
+            this.b[0].x = tempB[0].x;
+            this.b[0].y = tempB[0].y;
 
-        b[1].x = tempB[1].x;
-        b[1].y = tempB[1].y;
+            this.b[1].x = tempB[1].x;
+            this.b[1].y = tempB[1].y;
 
-        b[2].x = tempB[2].x;
-        b[2].y = tempB[2].y;
+            this.b[2].x = tempB[2].x;
+            this.b[2].y = tempB[2].y;
 
-        b[3].x = tempB[3].x;
-        b[3].y = tempB[3].y;
-    };
+            this.b[3].x = tempB[3].x;
+            this.b[3].y = tempB[3].y;
+        }
+    }
 
     public void getDirection1() {}
     public void getDirection2() {}
@@ -51,22 +54,22 @@ public class Mino {
     public void getDirection4() {}
 
     public void checkMovementCollision() {
-        leftCollision = false;
-        rightCollision = false;
-        bottomCollision = false;
+        this.leftCollision = false;
+        this.rightCollision = false;
+        this.bottomCollision = false;
 
         // Check frame collision
         // Left wall
         for (Block block : b) {
             if (block.x == PlayManager.left_x) {
-                leftCollision = true;
+                this.leftCollision = true;
                 break;
             }
         }
         // Right wall
         for (Block block : b) {
             if (block.x + Block.SIZE == PlayManager.right_x) {
-                rightCollision = true;
+                this.rightCollision = true;
                 break;
             }
         }
@@ -74,18 +77,46 @@ public class Mino {
         // Bottom floor
         for (Block block:b) {
             if (block.y + Block.SIZE == PlayManager.bottom_y) {
-                bottomCollision = true;
+                this.bottomCollision = true;
                 break;
             }
         }
     }
-    public void checkRotationCollision() {}
+    public void checkRotationCollision() {
+        this.leftCollision = false;
+        this.rightCollision = false;
+        this.bottomCollision = false;
+
+        // Check frame collision
+        // Left wall
+        for (Block block : tempB) {
+            if (block.x < PlayManager.left_x) {
+                this.leftCollision = true;
+                break;
+            }
+        }
+        // Right wall
+        for (Block block : tempB) {
+            if (block.x + Block.SIZE > PlayManager.right_x) {
+                this.rightCollision = true;
+                break;
+            }
+        }
+
+        // Bottom floor
+        for (Block block: tempB) {
+            if (block.y + Block.SIZE > PlayManager.bottom_y) {
+                this.bottomCollision = true;
+                break;
+            }
+        }
+    }
 
     public void update() {
 
         // Move the mino
         if (KeyHandler.upPressed) {
-            switch (direction) {
+            switch (this.direction) {
                 case 2 -> getDirection1();
                 case 3 -> getDirection2();
                 case 4 -> getDirection3();
@@ -97,30 +128,30 @@ public class Mino {
         checkMovementCollision();
 
         if (KeyHandler.downPressed) {
-            if (!bottomCollision) {
-                b[0].y += Block.SIZE;
-                b[1].y += Block.SIZE;
-                b[2].y += Block.SIZE;
-                b[3].y += Block.SIZE;
+            if (!this.bottomCollision) {
+                this.b[0].y += Block.SIZE;
+                this.b[1].y += Block.SIZE;
+                this.b[2].y += Block.SIZE;
+                this.b[3].y += Block.SIZE;
 
                 // When moved down, reset the autoDropCounter
-                autoDropCounter = 0;
+                this.autoDropCounter = 0;
             }
             KeyHandler.downPressed = false;
         }
 
         if (KeyHandler.leftPressed) {
-            if (!leftCollision) {
-                b[0].x -= Block.SIZE;
-                b[1].x -= Block.SIZE;
-                b[2].x -= Block.SIZE;
-                b[3].x -= Block.SIZE;
+            if (!this.leftCollision) {
+                this.b[0].x -= Block.SIZE;
+                this.b[1].x -= Block.SIZE;
+                this.b[2].x -= Block.SIZE;
+                this.b[3].x -= Block.SIZE;
             }
             KeyHandler.leftPressed = false;
         }
 
         if (KeyHandler.rightPressed) {
-            if (!rightCollision) {
+            if (!this.rightCollision) {
                 b[0].x += Block.SIZE;
                 b[1].x += Block.SIZE;
                 b[2].x += Block.SIZE;
@@ -129,14 +160,14 @@ public class Mino {
             KeyHandler.rightPressed = false;
         }
 
-        autoDropCounter++; // the counter increases in every frame
-        if (autoDropCounter == PlayManager.dropInterval) {
+        this.autoDropCounter++; // the counter increases in every frame
+        if (this.autoDropCounter == PlayManager.dropInterval) {
             // the mino goes down
-            b[0].y += Block.SIZE;
-            b[1].y += Block.SIZE;
-            b[2].y += Block.SIZE;
-            b[3].y += Block.SIZE;
-            autoDropCounter = 0;
+            this.b[0].y += Block.SIZE;
+            this.b[1].y += Block.SIZE;
+            this.b[2].y += Block.SIZE;
+            this.b[3].y += Block.SIZE;
+            this.autoDropCounter = 0;
         }
     }
 
