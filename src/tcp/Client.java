951adc3455569c93075f5dffeecce03a6780c6c9
jean luc kawel <20ka208@esisalama.org>
2024-01-13 14:12:36 +0200

    finish

---
+++ src/tcp/Client.java
@@ -0,0 +1,14 @@
+package tcp;
+
+import main.GamePanel;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Client {
+    public GamePanel client;
+
+    public InputStream inputStream;
+
+    public OutputStream outputStream;
+}
